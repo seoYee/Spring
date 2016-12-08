c964c7e50c762817755603340a356a579160df65
LeeseoYee <LeeseoYee@70-12-109-112.pools.spcsdns.net>
2016-12-08 21:30:49 +0900

    ConfigPart1 imports ConfigPart2.class

--- Ch05/src/main/java/config/ConfigPart1.java
+++ Ch05/src/main/java/config/ConfigPart1.java
@@ -2,11 +2,13 @@ package main.java.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import main.java.MemberDao;
 import main.java.MemberRegisterService;
 
 @Configuration
+@Import(ConfigPart2.class)
 public class ConfigPart1 {
 	
 	@Bean
