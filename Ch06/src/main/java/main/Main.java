fde01c7be804cc93a9b456770117de3137e030e0
LeeseoYee <LeeseoYee@70-12-109-112.pools.spcsdns.net>
2016-12-09 14:57:26 +0900

    initializing and destroying bean object

--- Ch06/src/main/java/main/Main.java
+++ Ch06/src/main/java/main/Main.java
@@ -2,12 +2,12 @@ package main.java.main;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import main.java.spring.Client;
+import main.java.spring.Client2;
 
 public class Main {
 	public static void main(String[] args) {
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:main/resource/springconf.xml");
-		Client client = ctx.getBean("client", Client.class);
+		Client2 client = ctx.getBean("client2", Client2.class);
 		client.send();
 		ctx.close();
 	}
