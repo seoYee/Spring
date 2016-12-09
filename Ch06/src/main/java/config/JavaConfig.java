0020be58000f550557a7d8faa06cb4e50178cd5c
LeeseoYee <LeeseoYee@70-12-109-112.pools.spcsdns.net>
2016-12-09 15:19:37 +0900

    scope of bean object using @Bean annotation

--- Ch06/src/main/java/config/JavaConfig.java
+++ Ch06/src/main/java/config/JavaConfig.java
@@ -2,12 +2,14 @@ package main.java.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import main.java.spring.Client2;
 
 @Configuration
 public class JavaConfig {
-	@Bean(initMethod = "connect", destroyMethod = "close")
+	@Bean
+	@Scope("prototype")
 	public Client2 client2(){
 		Client2 client2 = new Client2();
 		client2.setHost("서버 2");
