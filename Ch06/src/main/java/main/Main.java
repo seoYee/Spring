6bc35b1b92581e8a43fa8b972c613e070da73b49
LeeseoYee <LeeseoYee@70-12-109-112.pools.spcsdns.net>
2016-12-09 15:15:32 +0900

    initializing and destroying bean object through javaConfig.java

--- Ch06/src/main/java/main/Main.java
+++ Ch06/src/main/java/main/Main.java
@@ -1,15 +1,17 @@
 package main.java.main;
 
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import main.java.config.JavaConfig;
 import main.java.spring.Client2;
 
 public class Main {
-	public static void main(String[] args) {
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:main/resource/springconf.xml");
+	public static void main(String[] args) throws Exception {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(JavaConfig.class);
 		Client2 client = ctx.getBean("client2", Client2.class);
 		client.send();
-		ctx.close();
+		client.close();
 	}
 
 }
